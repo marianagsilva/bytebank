d0f9b88fbc2814efce23ee6ca8811ddccbb5a9eb
Mariana Godoy <marianagsilva@usp.br>
2021-02-23 08:58:45 -0300

    primeira versão do projeto bytebank pelo curso de orientação a objetos usando java

---
+++ src/TestaMetodo.java
@@ -0,0 +1,40 @@
+
+public class TestaMetodo {
+
+	public static void main(String[] args) {
+		Conta contaPaulo = new Conta();  
+		// Conta = classe(tipo) conta = variável
+		//tenho uma conta do tipo Conta
+		contaPaulo.saldo = 100;
+		contaPaulo.deposita(50);
+		System.out.println(contaPaulo.saldo);
+		
+		contaPaulo.deposita(10000);
+		
+		boolean conseguiuRetirar = contaPaulo.saca(20);
+		System.out.println(contaPaulo.saldo);
+		System.out.println(conseguiuRetirar);
+		
+		
+		Conta contaMarcela = new Conta();
+		contaMarcela.deposita(1000);
+		boolean sucessoTransferencia = contaMarcela.transfere(3000, contaPaulo);
+		if(sucessoTransferencia) {
+			System.out.println("transferencia com sucesso");
+		} else {
+			System.out.println("faltou dinheiro");
+		}
+		
+		contaMarcela.transfere(300, contaPaulo);
+		System.out.println(contaMarcela.saldo);
+		System.out.println(contaPaulo.saldo);
+		
+		contaPaulo.titular = "paulo silveira";
+		
+		
+		
+	}
+	
+	
+	
+}
---
+++ src/TesteReferencia.java
@@ -0,0 +1,35 @@
+
+public class TesteReferencia {
+	
+	public static void main (String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 300;
+		
+		System.out.println("saldo da primeira conta: " + primeiraConta.saldo);
+		
+		Conta segundaConta = primeiraConta; // fazem referencia para o mesmo objeto
+		                                   // mas são variáveis diferentes
+		System.out.println("saldo da segunda conta: " + segundaConta.saldo);
+		
+		segundaConta.saldo += 100;
+		System.out.println("novo saldo da segunda conta: " + segundaConta.saldo);
+		
+		System.out.println("novo saldo da primeira conta: " + primeiraConta.saldo);
+		
+		if(primeiraConta == segundaConta) {
+			System.out.println("sao a mesmissima conta");
+			
+		}else {
+			System.out.println("nao estao referenciando o mesmo objeto");
+		}
+				
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
